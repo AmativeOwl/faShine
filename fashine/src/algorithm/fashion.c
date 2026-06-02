0676a0d3212545b86fef0cb479ceda7b96ffeb8e
Claire Wong <cwon0185@student.monash.edu>
2026-06-02 14:00:49 +1000

    implementing a hash table to lookup compatible pairs for occasion scoring; implemented occasion scoring logic

--- fashine/src/algorithm/fashion.c
+++ fashine/src/algorithm/fashion.c
@@ -2,7 +2,34 @@
 #include <stdlib.h>
 #include <fashion.h>
 
-double score_colour(FashionItem item1, FashionItem item2, enum Adjective adj)
+#define NUM_OCCASION 6
+#define NUM_OCCASION_PAIRS 36
+
+typedef struct OccasionTable
+{
+    int hash;
+    double compatibilityScore;
+    struct OccasionTable *next;
+} OccasionTable;
+
+int hash(int item1Occasion, int item2Occasion)
+{
+    int min = (item1Occasion < item2Occasion) ? item1Occasion : item2Occasion;
+    int max = (item1Occasion > item2Occasion) ? item1Occasion : item2Occasion;
+
+    return abs(min) * NUM_OCCASION + abs(max);
+}
+
+void insert(OccasionTable **table, int hash, double compatibilityScore)
+{
+    OccasionTable *newOccasion = malloc(sizeof(OccasionTable));
+    newOccasion->compatibilityScore = compatibilityScore;
+    newOccasion->next = table[hash];
+    table[hash] = newOccasion;
+}
+
+double
+score_colour(FashionItem item1, FashionItem item2, enum Adjective adj)
 {
     HSL item1Colour = item1.colour, item2Colour = item2.colour;
 
@@ -39,6 +66,25 @@ double score_colour(FashionItem item1, FashionItem item2, enum Adjective adj)
 
 double score_occasion(FashionItem item1, FashionItem item2)
 {
+    int item1Occasion = item1.occasion, item2Occasion = item2.occasion;
+
+    if (item1Occasion == item2Occasion)
+        return 1.0;
+
+    static OccasionTable **table = NULL;
+    if (table == NULL)
+    {
+        table = calloc(NUM_OCCASION_PAIRS, sizeof(OccasionTable *));
+        insert(table, hash(CASUAL, ATHLETIC), 0.5);
+        insert(table, hash(SMART_CASUAL, CORPORATE), 0.5);
+        insert(table, hash(FORMAL, EVENING), 0.5);
+    }
+
+    int key = hash(item1Occasion, item2Occasion);
+    if (table[key])
+        return table[key]->compatibilityScore;
+
+    return 0;
 }
 
 double score_pair(FashionItem item1, FashionItem item2, enum Adjective adj)
